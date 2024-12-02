ff8238cc69a1cda1d674749671c1b92bd1f5a503
Santu1999 <123144992+Santu1999@users.noreply.github.com>
2024-12-02 13:10:51 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    role = db.Column(db.String(50), nullable=False, default="User")
+
+    def __init__(self, username, password, role):
+        self.username = username
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+        self.role = role
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password, password)
